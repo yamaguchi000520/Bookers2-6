1dd16eafcfd6430c3a0a0cbb8454b5a43dc17dda
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-03 07:23:51 +0000

    [Finish] いいね、コメントの非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,17 +4,11 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
-    # render :create
   end
 
   def destroy
-    # refroute = Rails.application.routes.recognize_path(request.referrer)
-    # @book = Book.find(params[:book_id])
-    # @BookComment.find(params[:id]).destroy
-    # @comment = BookComment.find_by(book_id: @book.id])
-    @comment = BookComment.find(params[:id]).
-    @comment.destroy
-    # render :destroy
+    @book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
   end
 
   private
